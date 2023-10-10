f0e8071a69705c19e1524f692d2ba29f82af754b
Kasun Thennakoon <tmkasun@gmail.com>
2023-10-09 22:45:04 -0400

    add google analytics

--- frontend/src/layouts/root.tsx
+++ frontend/src/layouts/root.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import Script from "next/script";
 import Drawer from "~/components/Drawer";
 import Footer from "~/components/Footer";
 
@@ -12,8 +13,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={`grow flex flex-col mx-1 sm:mx-6 gap-y-4 ${figtree.className}`}>
-      {children}
-      <Footer />
-    </div>);
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VGHH1YG3R2" />
+      <Script id="google-analytics">
+        {
+          `
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VGHH1YG3R2');
+        `
+        }
+      </Script>
+      <div className={`grow flex flex-col mx-1 sm:mx-6 gap-y-4 ${figtree.className}`}>
+        {children}
+        <Footer />
+      </div>
+    </>);
 }

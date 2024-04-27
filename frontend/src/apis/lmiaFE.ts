e96ed09cd5aca01f1f182d3f8f788dacb75363b1
kasun <tmkasun@gmail.com>
2024-04-27 16:13:11 -0400

    fix linter

--- frontend/src/apis/lmiaFE.ts
+++ frontend/src/apis/lmiaFE.ts
@@ -4,26 +4,26 @@ import { ILMIA, LMIAMetaData, LMIAResponseData } from "~/types/api";
 import { API_BASE_PATH } from "~/utils/utils";
 
 export const getLMIAs = async (context: QueryFunctionContext): Promise<LMIAResponseData> => {
-    const { queryKey } = context;
-    const [, payload] = queryKey;
-    const queryParams = new URLSearchParams(payload as any);
-    const apiEndpoint = `${API_BASE_PATH}/ircc/lmia?${queryParams}`;
-    const response = await fetch(apiEndpoint);
-    if (response.ok) {
-        const responseData = await response.json() as LMIAResponseData;
-        return responseData;
-    } else {
-        throw new Error("Error while fetching the data");
-    }
+  const { queryKey } = context;
+  const [, payload] = queryKey;
+  const queryParams = new URLSearchParams(payload as any);
+  const apiEndpoint = `${API_BASE_PATH}/ircc/lmia?${queryParams}`;
+  const response = await fetch(apiEndpoint);
+  if (response.ok) {
+    const responseData = (await response.json()) as LMIAResponseData;
+    return responseData;
+  } else {
+    throw new Error("Error while fetching the data");
+  }
 };
 
 export const getMetaData = async (context: QueryFunctionContext): Promise<any> => {
-    const apiEndpoint = `${API_BASE_PATH}/ircc/metadata`;
-    const response = await fetch(apiEndpoint);
-    if (response.ok) {
-        const responseData = await response.json() as LMIAMetaData;
-        return responseData;
-    } else {
-        throw new Error("Error while fetching the data");
-    }
-}
+  const apiEndpoint = `${API_BASE_PATH}/ircc/metadata`;
+  const response = await fetch(apiEndpoint);
+  if (response.ok) {
+    const responseData = (await response.json()) as LMIAMetaData;
+    return responseData;
+  } else {
+    throw new Error("Error while fetching the data");
+  }
+};

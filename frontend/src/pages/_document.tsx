5da6ec191c5cd227a58564d27b69871859394604
kasun <tmkasun@gmail.com>
2024-11-03 17:34:37 -0500

    update mobile

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,15 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="stylesheet" href="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html>
+            <Head>
+                <link rel="stylesheet" href="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }

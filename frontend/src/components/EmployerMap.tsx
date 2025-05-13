e3e9f7e678488838d50609393dfd8c369ffb62d7
kasun <tmkasun@gmail.com>
2025-05-13 15:14:28 -0400

    update error boundary

--- frontend/src/components/EmployerMap.tsx
+++ frontend/src/components/EmployerMap.tsx
@@ -2,6 +2,7 @@ import L, { LatLngTuple } from "leaflet";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { IByAddress } from "~/types/employerStats";
+import React from "react";
 interface IEmployerMap {
     addresses?: IByAddress[];
 }
@@ -16,45 +17,47 @@ const searchedLocationMarker = new LT.icon({
 });
 
 export const EmployerMap = (props: IEmployerMap) => {
-    const { addresses } = props;
+    const { addresses } = props || {};
     if (!addresses) return "No addresses found";
     const firstAddress = addresses[0];
     if (!firstAddress) return "First address is missing";
     const center = [addresses[0].geolocation?.lat, addresses[0].geolocation?.lon] as LatLngTuple;
     return (
-        <MapContainer tap={false} zoomControl={false} className="h-96" center={center} zoom={8} scrollWheelZoom>
-            {/* <ChangeView center={mapCenter} zoom={zoom} /> */}
-            <TileLayer
-                url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
-                accessToken="pk.eyJ1IjoidG1rYXN1biIsImEiOiJlNmZhOTYwNGJlODcxYWE5YjNmYjYzZmJiM2NlZWM4YiJ9.UT41ORairJ1PQ7woCnCH-A"
-                id="mapbox/streets-v11"
-                tileSize={512}
-                zoomOffset={-1}
-            />
+        <ErrorBoundary>
+            <MapContainer tap={false} zoomControl={false} className="h-96" center={center} zoom={8} scrollWheelZoom>
+                {/* <ChangeView center={mapCenter} zoom={zoom} /> */}
+                <TileLayer
+                    url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+                    accessToken="pk.eyJ1IjoidG1rYXN1biIsImEiOiJlNmZhOTYwNGJlODcxYWE5YjNmYjYzZmJiM2NlZWM4YiJ9.UT41ORairJ1PQ7woCnCH-A"
+                    id="mapbox/streets-v11"
+                    tileSize={512}
+                    zoomOffset={-1}
+                />
 
-            {addresses.map((address) => {
-                if (!address.geolocation) return null;
-                if (!address.geolocation.lat || !address.geolocation.lon) {
-                    console.error("Invalid geolocation", address);
-                    return null;
-                }
-                return (
-                    <Marker
-                        icon={searchedLocationMarker}
-                        key={address._id}
-                        position={[address?.geolocation?.lat, address?.geolocation?.lon]}
-                    >
-                        <Popup>
-                            <div>
-                                <p>{address._id}</p>
-                                <p>Total Positions: {address.totalPositions}</p>
-                                <p>Total LMIAs: {address.totalLMIAs}</p>
-                            </div>
-                        </Popup>
-                    </Marker>
-                );
-            })}
-        </MapContainer>
+                {addresses.map((address) => {
+                    if (!address.geolocation) return null;
+                    if (!address.geolocation.lat || !address.geolocation.lon) {
+                        console.error("Invalid geolocation", address);
+                        return null;
+                    }
+                    return (
+                        <Marker
+                            icon={searchedLocationMarker}
+                            key={address._id}
+                            position={[address?.geolocation?.lat, address?.geolocation?.lon]}
+                        >
+                            <Popup>
+                                <div>
+                                    <p>{address._id}</p>
+                                    <p>Total Positions: {address.totalPositions}</p>
+                                    <p>Total LMIAs: {address.totalLMIAs}</p>
+                                </div>
+                            </Popup>
+                        </Marker>
+                    );
+                })}
+            </MapContainer>
+        </ErrorBoundary>
     );
 };
 
---
+++ frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export class ErrorBoundary extends React.Component<{ children: React.ReactNode }> {
+    state = { hasError: false };
+
+    static getDerivedStateFromError() {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+        console.error("ErrorBoundary caught an error", error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
